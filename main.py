833cb9c43385a297d6229c25a4700e15f454d580
Rida Malik <internalprogression@gmail.com>
2025-10-05 05:56:59 +0500

    Contact Form now functional with SMTBLIB. Test send successfull.

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 import requests
+import os
+import smtplib
+from email.message import EmailMessage
+import dotenv
+dotenv.load_dotenv()
 
 posts = requests.get("https://api.npoint.io/83360257fa3216999b66").json()
 post_objects = []
@@ -24,9 +29,46 @@ if __name__ == '__main__':
 def about():
     return render_template('about.html', header_image=url_for('static', filename='assets/img/about-bg.jpg'))
 
-@app.route('/contact')
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
-    return render_template('contact.html', header_image=url_for('static', filename='assets/img/contact-bg.jpg'))
+    if request.method == 'POST':
+        data = request.form
+        name = data["name"]
+        email = data["email"]
+        phone = data["phone"]
+        message = data["message"]
+
+        # Email setup
+        EMAIL_ADDRESS = os.environ.get('BLOG_EMAIL_USER')
+        EMAIL_PASSWORD = os.environ.get('BLOG_EMAIL_PASS')
+        RECEIVER_EMAIL = email
+
+        email_message = EmailMessage()
+        email_message['Subject'] = f'New Contact Form Submission from {name}'
+        email_message['From'] = EMAIL_ADDRESS
+        email_message['To'] = EMAIL_ADDRESS
+        email_message.set_content(f"""
+Name: {name}
+Email: {email}
+Phone: {phone}
+Message: {message}
+        """)
+
+        try:
+            with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+                smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+                smtp.send_message(email_message)
+        except Exception as e:
+            print(f"Error sending email: {e}")
+            return render_template('contact.html', header_image=url_for('static', filename='assets/img/contact-bg.jpg'), header_title="Error sending message", header_subtitle="Please try again later.")
+
+        return render_template('contact.html', header_image=url_for('static', filename='assets/img/contact-bg.jpg'), header_title="Successfully sent message", header_subtitle="Thank you for reaching out!")
+    else:
+        return render_template('contact.html', header_image=url_for('static', filename='assets/img/contact-bg.jpg'), header_title="Contact Me", header_subtitle="Have questions? I have answers.")
+
+
+
+
 
 @app.route('/post/<int:index>')
 def show_post(index):
@@ -35,3 +77,4 @@ def show_post(index):
         if post["id"] == index:
             requested_post = post
     return render_template('post.html', header_image=url_for('static', filename='assets/img/post-bg.jpg'), post=requested_post)
+
